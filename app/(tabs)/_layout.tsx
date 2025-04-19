647b3327631966e5c5db8380754c83ac1fbadabe
石原 祐希 <issi0430bjc@gmail.com>
2025-04-19 12:15:57 +0900

    画面名を修正

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -14,7 +14,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'インデックス',
+          title: 'お祝いリスト',
           headerTintColor: theme.Text.primary,
           headerStyle: {
             backgroundColor: theme.Backgroud.primary,
